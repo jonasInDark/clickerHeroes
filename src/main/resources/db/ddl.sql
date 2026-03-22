d2529d77d91ded9b984d3f645e211401166eb642
8c8c8c8c8c8 <8c8c8c8c8c8@gmail.com>
2026-03-22 22:52:58 +0900

    feat(ddl): update ddl
    - add cascade
    - monsters: remove name field

--- src/main/resources/db/ddl.sql
+++ src/main/resources/db/ddl.sql
@@ -19,13 +19,13 @@ CREATE TABLE users
 CREATE TABLE heroes
 (
     id                  serial primary key,
-    name                varchar(100)          not null,
-    details             varchar(255)          not null,
-    base_dps_mantissa   float   DEFAULT 1.000 not null,
-    base_dps_exponent   integer DEFAULT 1     not null,
-    base_price_mantissa float   DEFAULT 1.000 not null,
-    base_price_exponent integer DEFAULT 1     not null,
-    created_at          timestamptz           not null
+    name                varchar(20)  not null,
+    details             varchar(255) not null,
+    base_dps_mantissa   float        not null,
+    base_dps_exponent   integer      not null,
+    base_price_mantissa float        not null,
+    base_price_exponent integer      not null,
+    created_at          timestamptz  not null
 );
 
 CREATE TABLE monsters
@@ -34,46 +34,47 @@ CREATE TABLE monsters
     user_id     integer               NOT NULL,
     hp_mantissa float   DEFAULT 1.000 not null,
     hp_exponent integer DEFAULT 1     not null,
-    is_boss     boolean DEFAULT false not null,
-    created_at  timestamptz           not null
+    created_at  timestamptz           not null,
+    updated_at  timestamptz           not null
 );
 
 CREATE TABLE gold
 (
     ID            serial primary key,
-    user_id       integer               NOT NULL,
-    gold_mantissa float   DEFAULT 1.000 not null,
-    gold_exponent integer DEFAULT 1     not null,
-    created_at    timestamptz           not null,
-    updated_at    timestamptz           not null
+    user_id       integer     NOT NULL,
+    gold_mantissa float       not null,
+    gold_exponent integer     not null,
+    created_at    timestamptz not null,
+    updated_at    timestamptz not null
 );
 
 CREATE TABLE hero_skills
 (
     id                     serial primary key,
-    heroes_id              integer      NOT NULL,
-    name                   varchar(100) not null,
+    hero_id                integer      NOT NULL,
+    name                   varchar(50)  not null,
     details                varchar(255) not null,
     required_level         integer      not null,
     required_gold_mantissa float        not null,
     required_gold_exponent integer      not null,
-    skill_type             varchar(10)  not null check ( skill_type in
-                                                         ('DPS_ACTIVE', 'DPS_PASSIVE', 'GOLD_ACTIVE', 'GOLD_PASSIVE') ),
+    skill_type             varchar(20)  not null check ( skill_type in
+                                                         ('DPS_ACTIVE', 'DPS_PASSIVE',
+                                                          'GOLD_ACTIVE', 'GOLD_PASSIVE') ),
     created_at             timestamptz  not null
 );
 
 CREATE TABLE hired_heroes
 (
     id             serial primary key,
-    user_id        integer               NOT NULL,
-    hero_id        integer               NOT NULL,
-    dps_mantissa   float   DEFAULT 1.000 not null,
-    dps_exponent   integer DEFAULT 1     not null,
-    price_mantissa float   DEFAULT 1.000 not null,
-    price_exponent integer DEFAULT 1     not null,
-    level          integer               not null,
-    created_at     timestamptz           not null,
-    updated_at     timestamptz           not null
+    user_id        integer     NOT NULL,
+    hero_id        integer     NOT NULL,
+    dps_mantissa   float       not null,
+    dps_exponent   integer     not null,
+    price_mantissa float       not null,
+    price_exponent integer     not null,
+    level          integer     not null,
+    created_at     timestamptz not null,
+    updated_at     timestamptz not null
 );
 
 create table activated_hero_skills
@@ -88,38 +89,34 @@ create table activated_hero_skills
 
 alter table activated_hero_skills
     add constraint fk_activated_hero_skills_hired_hero_id
-        foreign key (hired_hero_id) references hired_heroes (id),
+        foreign key (hired_hero_id) references hired_heroes (id) on delete cascade,
     add constraint fk_activated_hero_skills_hero_skill_id
         foreign key (hero_skill_id) references hero_skills (id),
     add constraint uk_activated_hero_skills_hired_hero_skill unique (hired_hero_id, hero_skill_id);
 
 alter table users
-    add constraint uk_users_name unique (name),
-    add constraint uk_users_password unique (password);
+    add constraint uk_users_name unique (name);
 
 alter table heroes
     add constraint uk_heroes_name unique (name);
 
 alter table monsters
     add constraint fk_monsters_user_id
-        foreign key (user_id) references users (id),
+        foreign key (user_id) references users (id) on delete cascade,
     add constraint uk_monsters_user_id unique (user_id);
-alter table monsters
-    add constraint uk_monsters_name unique (name);
 
 alter table gold
     add constraint fk_gold_user_id
-        foreign key (user_id) references users (id),
+        foreign key (user_id) references users (id) on delete cascade,
     add constraint uk_gold_user_id unique (user_id);
 
 alter table hero_skills
     add constraint fk_hero_skills_heroes_id
-        foreign key (heroes_id) references heroes (id);
-alter table hero_skills
-    add constraint uk_hero_skills_name unique (name);
+        foreign key (hero_id) references heroes (id);
 
 alter table hired_heroes
     add constraint fk_hired_heroes_user_id
-        foreign key (user_id) references users (id),
+        foreign key (user_id) references users (id) on delete cascade,
     add constraint fk_hired_heroes_hero_id
-        foreign key (hero_id) references heroes (id);
+        foreign key (hero_id) references heroes (id),
+    add constraint uk_hired_heroes_user_hero_id unique (user_id, hero_id);
